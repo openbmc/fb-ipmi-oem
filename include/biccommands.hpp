32d79c2f131bd79f7948affc40771315c5277640
Kumar Thangavel <thangavel.k@hcl.com>
2020-06-11 10:31:07 +0530

    fb-ipmi-oem: yosemitev2: Add Ipmi implementation to handle BIC request
    Added Implementation to handle BIC Request from host to BMC and
    send the response back to host. We are able to see the BMC
    information in the host BIOS.
    
    TESTED : Built Facebook YosemiteV2 images and loaded on
    the target hardware. We are able to see BMC information
    on two host's BIOS.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: I7586426809973b5d670d45495daf76e91b82ff23

---
+++ include/biccommands.hpp
@@ -0,0 +1,13 @@
+
+#define SIZE_IANA_ID 3
+#define DATA_BYTE_IDX 6
+
+#define INTERFACE_IDX 3
+#define NETFN_IDX 4
+#define CMD_IDX 5
+#define SHIFT_TWO 2
+
+#define ZERO_IDX 0
+#define ONE_IDX 1
+
+constexpr uint8_t cmdOemBicInfo = 0x01;
