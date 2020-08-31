65e04d18d9d3ce1b9509e94afa7a2c7422c0c72e
Kumar Thangavel <thangavel.k@hcl.com>
2020-08-31 22:27:33 +0530

    fb-ipmi-oem: yosemitev2: Add Ipmi implementation for read post codes
    Added implementation to handle read post code BIC request from host to BMC and
    send the response back to host.
    
    TESTED : Built Facebook YosemiteV2 images and loaded on the target hardware.
    Verified all the host's postcodes in BMC.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: Iee9a224b381db5ab9e244b9ae2999a602e520a36

--- src/biccommands.cpp
+++ src/biccommands.cpp
@@ -23,6 +23,7 @@
 #include <phosphor-logging/log.hpp>
 
 #include <vector>
+#include <variant>
 #include <iostream>
 
 namespace ipmi
@@ -65,6 +66,47 @@ ipmi::RspType<std::array<uint8_t, 3>, uint8_t, uint2_t, uint6_t, uint8_t,
                                  res->cc, res->payload);
 }
 
+//----------------------------------------------------------------------
+// ipmiOemPostBufferHandler (CMD_OEM_BIC_POST_BUFFER_INFO)
+// This Function will handle BIC incomming postcode from multi-host for
+// netfn=0x38 and cmd=0x08 send the response back to the sender.
+//----------------------------------------------------------------------
+
+ipmi::RspType<std::array<uint8_t, 3>, uint8_t>
+    ipmiOemPostBufferHandler(ipmi::Context::ptr ctx,
+                             std::array<uint8_t, 3> iana, uint8_t interface,
+                             std::vector<uint8_t> data)
+{
+    // creating bus connection
+    std::shared_ptr<sdbusplus::asio::connection> conn = getSdBus();
+
+    // storing post code as varaint
+    std::variant<uint64_t> postCode = static_cast<uint64_t>(data.at(0));
+
+    // creating dbus objects for 1 to N process
+    std::string dbusObj = "/xyz/openbmc_project/state/boot/raw" +
+                          std::to_string((ctx->channelIdx + 1));
+
+    // creating method call to update postd value
+    auto method = conn->new_method_call(
+        "xyz.openbmc_project.State.Boot.Raw", dbusObj.c_str(),
+        "org.freedesktop.DBus.Properties", "Set");
+
+    // Adding paramters to method call
+    method.append("xyz.openbmc_project.State.Boot.Raw", "Value", postCode);
+
+    // Invoke method call function
+    auto reply = conn->call(method);
+    if (reply.is_method_error())
+    {
+        phosphor::logging::log<phosphor::logging::level::ERR>(
+            "Error in set Value of postd Raw interface");
+    }
+
+    // sending the response with headers
+    return ipmi::responseSuccess(iana, interface);
+}
+
 static void registerBICFunctions(void)
 {
 
@@ -74,6 +116,9 @@ static void registerBICFunctions(void)
     ipmi::registerHandler(ipmi::prioOpenBmcBase, ipmi::netFnOemFive,
                           cmdOemBicInfo, ipmi::Privilege::User,
                           ipmiOemBicHandler);
+    ipmi::registerHandler(ipmi::prioOpenBmcBase, ipmi::netFnOemFive,
+                          cmdOemSendPostBufferToBMC, ipmi::Privilege::User,
+                          ipmiOemPostBufferHandler);
     return;
 }
 
