d93efe105411275d7158edac057552b1b93abcd7
Kumar Thangavel <thangavel.k@hcl.com>
2020-08-31 22:27:33 +0530

    fb-ipmi-oem: yosemitev2: Add Ipmi implementation for read post codes
    Added implementation to handle read post code BIC request from host to BMC and
    send the response back to host.
    
    TESTED : Built Facebook YosemiteV2 images and loaded on the target hardware.
    Verified all the host's postcodes in BMC.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: Iee9a224b381db5ab9e244b9ae2999a602e520a36

--- include/biccommands.hpp
+++ include/biccommands.hpp
@@ -1,3 +1,10 @@
 
 // Command for getting device id
 constexpr uint8_t cmdOemBicInfo = 0x01;
+
+// Command for getting post code
+constexpr uint8_t cmdOemSendPostBufferToBMC = 0x08;
+
+const char* dbusObj = "/xyz/openbmc_project/state/boot/raw";
+
+const char* dbusService = "xyz.openbmc_project.State.Boot.Raw";
