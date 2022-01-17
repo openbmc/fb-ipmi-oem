cbd603e82b3c58e2e138d43f987d1a9f9d706254
Karthikeyan Pasupathi <p_karthikeya@hcl.com>
2022-01-17 14:30:02 +0530

    yosemitev2:Multi-host CPU info displayed in OCP debug-card.
    This implementation is going to display the processor
    information in the OCP debug-card.
    
    Tested: information frame value of processor info
    displayed in the OCP debug card with multi-host
    Yosemitev2 platform.
    
    Signed-off-by: Karthikeyan Pasupathi <p_karthikeya@hcl.com>
    Change-Id: Iec29bae79a4c56d92184a69ea187549895f34db9

--- src/usb-dbg.cpp
+++ src/usb-dbg.cpp
@@ -21,7 +21,6 @@ namespace ipmi
 
 ipmi_ret_t getNetworkData(uint8_t lan_param, char* data);
 int8_t getFruData(std::string& serial, std::string& name);
-void sysConfig(std::vector<std::string>& data, size_t pos);
 
 bool isMultiHostPlatform();
 
@@ -123,7 +122,7 @@ int frame::init(size_t size)
 // return 0 on seccuess
 int frame::append(const char* string, int indent)
 {
-    const size_t buf_size = 64;
+    const size_t buf_size = 128;
     char lbuf[buf_size];
     char* ptr;
     int ret;
@@ -1041,6 +1040,11 @@ static int udbg_get_info_page(uint8_t, uint8_t page, uint8_t* next,
             {
                 frame_info.append(info.c_str(), 1);
             }
+
+            // Processor info
+            std::string result;
+            procInfo(result, pos);
+            frame_info.append(result.c_str(), 1);
         }
 
     } // End of update frame
