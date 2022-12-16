702ec6989cd3535d068b1131d4c1eb9f359dfe4c
Bonnie Lo <Bonnie_Lo@wiwynn.com>
2022-12-16 14:09:21 +0800

    IPMI OEM Get BIC GPIO State
    Register an IPMI command handler - NetFn: 0x38 Cmd: 0x03.
    Send command through IPMB to get BIC GPIO state.
    Add Meta IANA definition for future utilization.
    
    Limitation:
    1. Could only get the state of BIC GPIOs which are configured as GPIO
    2. Couldn't get all BIC GPIO states
    
    Test Case:
    Call IPMI OEM get BIC GPIO state
    
    Signed-off-by: Bonnie Lo <Bonnie_Lo@wiwynn.com>
    Change-Id: Idda8d4c532b2971c14662e7b34ea101cf584fa22

--- include/biccommands.hpp
+++ include/biccommands.hpp
@@ -1,9 +1,10 @@
 
-// Command for getting device id
-constexpr uint8_t cmdOemBicInfo = 0x01;
-
-// Command for getting post code
-constexpr uint8_t cmdOemSendPostBufferToBMC = 0x08;
+enum class fb_bic_cmds : uint8_t
+{
+    CMD_OEM_BIC_INFO = 0x01,
+    CMD_OEM_GET_BIC_GPIO_STATE = 0x03,
+    CMD_OEM_SEND_POST_BUFFER_TO_BMC = 0x08,
+};
 
 const char* dbusObj = "/xyz/openbmc_project/state/boot/raw";
 
