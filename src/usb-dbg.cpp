9c697e0e1eba6b0232efb918629137127ceec605
Potin Lai <potin.lai@quantatw.com>
2024-08-28 13:40:37 +0800

    usb-dbg: include extra code as part of postcode
    Catalina system stores the first 8 bytes postcode in to primary code and
    store last one byte into extra code array.
    
    Include extra code as part of postcode to display full postcode on debug
    card screen.
    
    Change-Id: I4e4b40a254e90e0b4ae6d4ace16a93526f3f4c6c
    Signed-off-by: Potin Lai <potin.lai@quantatw.com>

--- src/usb-dbg.cpp
+++ src/usb-dbg.cpp
@@ -898,10 +898,13 @@ static int udbg_get_postcode(uint8_t, uint8_t page, uint8_t* next,
             reply.read(postcodes);
 
             // Insert retrieved postcodes into frame_postcode
-            std::string result;
             for (const auto& [code, extra] : postcodes)
             {
-                result = std::format("{:02x}", code);
+                std::string result = std::format("{:02x}", code);
+                for (const auto& byte : extra)
+                {
+                    result += std::format("{:02x}", byte);
+                }
                 frame_postcode.append(result.c_str(), 0);
             }
         }
